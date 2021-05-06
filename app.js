2e7205044f47bba5d0448a761f129dc3476a6f4c
Henrique de Castro <hendecastro@gmail.com>
2021-05-06 12:23:18 -0300

    Configurado bodyparser e handlebars

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 // const mongoose = require('mongoose')
 
+const app = express()
+
 // Settings......................................................
+    // Body-Parser...............................................
+    app.use(bodyParser.urlencoded({extended: true}))
+    app.use(bodyParser.json())
+
+    // HandleBars................................................
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+
+    // Mongoose..................................................
+        // todo#
 
 // Routes........................................................
 
 // Others........................................................
 
 //Server.........................................................
+const PORT = 8081
+
+app.listen(PORT, (req, res)=>{
+    console.log(`server running on port http://localhost:${PORT}`)
+})
