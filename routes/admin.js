d664f55522ea39643b359c19b68fccb714c26fc7
Henrique de Castro <hendecastro@gmail.com>
2021-05-06 22:45:05 -0300

    Feito editar categoria

--- routes/admin.js
+++ routes/admin.js
@@ -14,10 +14,7 @@ router.get('/posts', (req, res)=>{
 
 router.get('/categorias', (req, res)=>{
     Categoria.find().lean().sort({date: 'desc'}).then((categorias)=>{
-
         res.render('admin/categorias', {categorias: categorias})
-        console.log(categorias)
-
     }).catch((err)=>{
         req.flash('error_msg', `Houve um erro ao carregar as categorias`)
         res.render('admin/categorias')
@@ -58,6 +55,56 @@ router.post('/categorias/nova', (req, res)=>{
             res.redirect('/admin/categorias')
         }).catch((err)=>{
             req.flash('error_msg', `Houve um erro ao criar a categoria ${req.body.nome}`)
+            res.redirect('/admin/categorias')
+        })
+    }
+})
+
+router.get('/categorias/edit/:id', (req, res)=>{
+    Categoria.findOne({_id: req.params.id}).lean()
+    .then((categorias)=>{
+        res.render('admin/editcategorias', {categorias: categorias})
+    }).catch((err)=>{
+        req.flash('error_msg', 'Esta categoria não existe!')
+        res.redirect('/admin/categorias')
+    })
+})
+
+router.post('/categorias/edit', (req, res) => {
+    var erros = []
+
+    if (!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null) {
+        erros.push({
+            text: "Nome Inválido!"
+        })
+    }
+
+    if (!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null) {
+        erros.push({
+            text: "Slug Inválido!"
+        })
+    }
+
+    if (erros.length > 0) {
+        res.render('admin/editcategorias', {erros: erros})
+    } else {
+    
+        Categoria.findOne({_id: req.body.id})
+        .then((categorias)=>{
+
+            categorias.nome = req.body.nome
+            categorias.slug = req.body.slug
+
+            categorias.save().then(()=>{
+                req.flash('success_msg', `Categoria ${req.body.nome} editada com sucesso!`)
+                res.redirect('/admin/categorias')
+            }).catch((err)=>{
+                req.flash('error_msg', `Houve um erro ao salvar a atualização da categoria ${req.body.nome}`)
+                res.redirect('/admin/categorias')
+            })
+
+        }).catch((err)=>{
+            req.flash('error_msg', `Houve um erro ao editar a categoria ${req.body.nome}`)
             res.redirect('/admin/categorias')
         })
     }
